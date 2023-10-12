8f9caed427497e0cb369790a61041a6e69b7060e
Luan Himmlisch <luan@himmlisch.com.mx>
2023-10-12 17:00:51 -0600

    Added NIP 02, 03, 04 specifications

--- src/Enums/TagName.php
+++ src/Enums/TagName.php
@@ -7,4 +7,7 @@ enum TagName: string
     case Event = 'e';
     case User = 'p';
     case ReplaceableEvent = 'a';
+
+        // NIP-03
+    case Block = 'block';
 }
--- src/Types/Kind.php
+++ src/Types/Kind.php
@@ -7,4 +7,13 @@ readonly final class Kind
     // General kinds
     const METADATA = 0;
     const TEXT_NOTE = 1;
+
+    // NIP-02
+    const CONTACT_LIST = 3;
+
+    // NIP-03
+    const OPEN_TIMESTAMPS = 1040;
+
+    // NIP-04
+    const ENCRYPTED_DIRECT_MESSAGE = 4;
 }
